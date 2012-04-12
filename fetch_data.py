33c985280b6babc8d1967a69af97d4d0a30e730d
Isak Wiström <isak.wistrom@gmail.com>
2012-04-12 15:40:27 +0200

    added spec

--- fetch_data.py
+++ fetch_data.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 import mechanize
 import os.path
+import logging
+import sys
+from optparse import OptionParser
 
 # 1. do search on dressage disipline
 # 2. get number of page results
@@ -182,6 +185,7 @@ def competitions( url ):
         #date
         #print row.contents[4].contents[0]
         if row.contents[7].a:
+  
             javascript = row.contents[7].a['href'].split('"')
             print javascript[1] # so this is the postback
         #    print row.contents[7].a['href']
@@ -189,7 +193,85 @@ def competitions( url ):
             
     return None
 
-def results( url ):
+def results( url, page ):
+    
+    logger = logging.getLogger("mechanize")
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    logger.setLevel(logging.INFO)
+    
+    br = mechanize.Browser()
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    
+    br.set_debug_redirects(True)
+    br.set_handle_redirect(True)
+    br.open(url)
+    
+    br.select_form(nr=0)
+    br.set_all_readonly(False)
+    
+    br.form["__EVENTTARGET"]=page
+    br.form["__EVENTARGUMENT"]=""
+    
+    br.find_control("ctl00$PlaceHolderMain$btnReset").disabled = True
+    
+    br.submit()
+
+    # get result page
+
+    initialresp = br.response().read()
+
+    soup = BeautifulSoup( initialresp )
+    
+    print soup.title.contents[0].strip()
+    
+    ## get table row titles
+    
+    headers = []
+    
+    columns = soup.select(".headerr")
+    
+    for column in columns:
+        name = column.contents[0].strip()
+        if len(name) != 0:
+            print name
+    
+    ## get judges
+    
+    #PlaceHolderMain_fvDetail_ucDressageJudges_panJudges
+    
+    judges = soup.find(id='PlaceHolderMain_fvDetail_ucDressageJudges_panJudges')
+    
+    tds = judges.find_all('td')
+    
+    for td in tds:
+        print td
+    
+    #print judges.td
+    
+    
+    ## get table results
+    
+    results = []
+    
+    firstrows = soup.select(".row")
+    secondrows = soup.select(".altrow")
+    
+    rows = firstrows + secondrows
+    
+    for row in rows:
+        
+        print row.contents[1].a['title'] # position
+        print row.contents[3].a.contents[0].strip() # competitor
+        print row.contents[4].a.contents[0].strip() # horse 
+        print row.contents[5].contents[0].strip() # prize money 
+        
+        print row.contents[7].contents[0].strip()
+        print row.contents[8].contents[0].strip()
+        print row.contents[9].contents[0].strip()
+        print row.contents[10].contents[0].strip()
+        #print row.contents[11].contents[0].strip()
+        print row.contents[12].contents[0].strip()
+    
     return None
 
 def judgegender( url, judges ):
@@ -209,7 +291,10 @@ def main():
     
     #print len(myevents)
     
-    mycompetitions = competitions( "http://search.fei.org/Search_Centre/Calendar/Pages/EventDetail.aspx?p=A7875F775FD231321FD325260B54B2EE" )
+    #mycompetitions = competitions( "http://search.fei.org/Search_Centre/Calendar/Pages/EventDetail.aspx?p=A7875F775FD231321FD325260B54B2EE" )
+    
+    #https://data.fei.org/Calendar/EventDetail.aspx?p=110BD1B7EAB6F342BA1E0ED0CD48D802
+    mycompetitions = results( "https://data.fei.org/Calendar/EventDetail.aspx?p=110BD1B7EAB6F342BA1E0ED0CD48D802", "ctl00$PlaceHolderMain$gvCompetitions$ctl04$lbIndivResults" )
     
     #for evt in myevents:
     #    print evt.title
